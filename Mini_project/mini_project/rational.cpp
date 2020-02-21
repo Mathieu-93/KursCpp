b29ca1a3fd2fa84e5af5c86270d3eda0a4e7b2cc
Mathieu_93 <ogonowskimatuesz@yahoo.pl>
2020-02-21 11:55:32 +0100

    #5 MO Rational class

--- Mini_project/mini_project/rational.cpp
+++ Mini_project/mini_project/rational.cpp
@@ -5,7 +5,7 @@ class Rational
 {
 public:
     Rational (int numerator, int denominator =1): numer{numerator}, denom{denominator}{
-        this ->normalized(numer,denom);
+       // this ->normalized(numer,denom);
     }
     ~Rational(){}
     Rational( const Rational& copy){
@@ -28,30 +28,56 @@ public:
     int numerator () const{return numer; }
     int denominator () const { return denom; }
 
-//    static Rational normalize(Rational const& other){
-//        if(other.numer<0 && other.denom<0){
-//            return Rational{-other.numer, -other.numer};
-//        }
+    static Rational normalize(Rational const& other){
+        int denoma = other.denominator();
+        int numera = other.numerator();
+        //int gcd_ = gcd(numera,denoma);
 
-//    }
-
-    static Rational normalized(int numerator,  int denominator){
-        //gcd of two non-negative numbers:  Euclid's algorithm.
-//        do {
-//            if(numerator<denominator){
-//                int temp = numerator;
-//                numerator = denominator;
-//                denominator = temp;
-//            }
-//            numerator-=denominator;
-//        }while(numerator);
-//        gcdND= denominator;
-//        return *this;
-        if(numerator<0 && denominator<0){
-            numerator = -numerator;
-            denominator= -denominator;
+        if(other.numerator()<0&&other.denominator()<0){
+            numera = -other.numerator();
+            denoma = -other.denominator();
         }
-        return Rational {numerator, denominator} ;
+        else if((other.numerator()<0) != (other.denominator()<0)) {
+            if (other.denominator()<0){numera = -other.numerator(), denoma =-other.denominator();}
+            else{numera = other.numerator(), denoma = other.denominator();}
+
+        }
+        else{
+            numera = other.numerator();
+            denoma = other.denominator();
+        }
+        return Rational{numera, denoma};
+
+    }
+    int gcd (int numerator, int denominator){
+        //gcd of two non-negative numbers:  Euclid's algorithm.
+
+        do {
+                    if(numerator<denominator){
+                        int temp = numerator;
+                        numerator = denominator;
+                        denominator = temp;
+                    }
+                    numerator-=denominator;
+                }while(numerator);
+        return denominator;
+    }
+
+
+    static Rational normalized(int numerator, int denominator){
+        //gcd of two non-negative numbers:  Euclid's algorithm.
+        int numera=numerator;
+        int denomi= denominator;
+        do {
+            if(numera<denomi){
+                int temp = numera;
+                numera = denomi;
+                denomi = temp;
+            }
+            numera-=denomi;
+        }while(numera);
+        int gcdND= denomi;
+        return Rational {numerator/gcdND, denominator/gcdND} ;
 
     }
 
@@ -98,7 +124,7 @@ public:
 private:
     int numer;
     int denom;
-    int gcdND;
+    //int gcdND;
 
     void initialize(int numerator, int denominator);
 
@@ -135,19 +161,21 @@ Rational operator/(int i, Rational const& r){
 }
 
 int main(){
-    Rational rr{-2,-3};
-    //Rational r();
-    //r.normalize(rr);
-    cout<<"rr ="<<rr<<endl;
+    Rational rr{15,-25};
+    Rational irr=Rational::normalized(15,25);
+
+    cout<<"irr ="<<irr<<endl;
     Rational r2(2, 11), r3(1, -3), r5(18, 6);
     Rational res1 = 3 + r2 * r3;
+    //Rational ires1 =Rational::normalize(res1);
     Rational res2 = (3 + r2) * r3;
     Rational res3 = 3 + r3 * (r2 + 2) / (r5 - r3);
+   // Rational ires3 = Rational::normalized(res3.numerator(),res3.denominator());
 
-    cout << 3 << " + " << r2 << " * " <<  r3 << " = " << res1 << endl;
-    cout << "(" << 3 << " + " << r2 << ")" << " * " <<  r3 << " = " << res2 << endl;
+    cout << 3 << " + " << r2 << " * " <<  r3 << " = " << Rational::normalize(res1) << endl;
+    cout << "(" << 3 << " + " << r2 << ")" << " * " <<  r3 << " = " << Rational::normalize(res2) << endl;
     cout << 3 << " + " << r3 << " * " << "(" << r2 << " + " << 2 << ")" << "/"
-         << "(" << r5 << " - " << r3 << ")" << " = " << res3 << endl;
+         << "(" << r5 << " - " << r3 << ")" << " = " << Rational ::normalized(res3.numerator(),res3.denominator()) << endl;
 
     return 0;
 }
